c4ad81ea414102183468c39714dffd497f185fbb
fakuzatsu <abyerswilson@gmail.com>
2023-11-27 21:41:34 +0000

    allow forgetting HMs

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -2360,12 +2360,16 @@ static void Task_HandleReplaceMoveInput(u8 taskId)
 
 static bool8 CanReplaceMove(void)
 {
+    #if P_FORGET_HMS == TRUE
+        return TRUE;
+    #else 
     if (sMonSummaryScreen->firstMoveIndex == MAX_MON_MOVES
         || sMonSummaryScreen->newMove == MOVE_NONE
         || IsMoveHM(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]) != TRUE)
         return TRUE;
     else
         return FALSE;
+    #endif
 }
 
 static void ShowCantForgetHMsWindow(u8 taskId)
