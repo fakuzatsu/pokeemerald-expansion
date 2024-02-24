658423a4939de6c8e528dde5422028890038866f
fakuzatsu <abyerswilson@gmail.com>
2024-02-24 16:31:40 +0000

    added Light of Ruin test

---
+++ test/battle/move_effect/light_of_ruin.c
@@ -0,0 +1,24 @@
+#include "global.h"
+#include "test/battle.h"
+
+ASSUMPTIONS
+{
+    ASSUME(gBattleMoves[MOVE_LIGHT_OF_RUIN].effect == EFFECT_MEGA_DOUBLE_DMG);
+}
+
+SINGLE_BATTLE_TEST("Mega Pokemon take double damage from Light of Ruin", s16 damage)
+{
+    bool32 megaEvolved;
+    PARAMETRIZE { megaEvolved = FALSE; }
+    PARAMETRIZE { megaEvolved = TRUE; }
+    GIVEN {
+        PLAYER(SPECIES_VENUSAUR) { Item(ITEM_VENUSAURITE); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_TACKLE, megaEvolve: megaEvolved); MOVE(opponent, MOVE_LIGHT_OF_RUIN); }
+    } SCENE {
+        HP_BAR(player, captureDamage: &results[i].damage);
+    } FINALLY {
+        EXPECT_MUL_EQ(results[0].damage, UQ_4_12(2.0), results[1].damage);
+    }
+}
